3a0f671aebce7d332a58844903b51ec6948bb128
miltonlab <1000ton.lab@gmail.com>
2012-11-04 23:42:06 -0500

    modificando instalador windows

--- install/win32/install.bat
+++ install/win32/install.bat
@@ -5,9 +5,7 @@ echo Si no esta seguro de iniciar la instalacion de 'forestweb' presione Ctrl+c
 pause
 
 REM #=======================================================
-
-REM # Instalacion.
-
+REM # Instalacion del codigo de la Aplicacion
 REM #=======================================================
 
 mkdir C:\forestweb
@@ -16,15 +14,26 @@ xcopy /E ..\..\src\forestweb\ C:\forestweb
 
 echo ::: COPIADA LA APLICACION 'forestweb' en C:
 
-copy "forestal.bat" "%USERPROFILE%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\"
+REM #=========================================================
+REM # Instalacion de la Base de Datos
+REM #==========================================================
+
+cd C:\forestweb\mongodb\win32\
+
+mongorestore --dbpath data C:\forestweb\data\mongobackup
+
+echo ::: Generada la Base de datos. OK
+
+# Pendiente iniciar servidor de BD
+mongo localhost:27019/admin --quiet --eval "db.addUser('mongoadmin','humongous')"
+
+echo ::: Agregado el usuario administrador de BD. OK
 
 REM #=========================================================
-Instalacion de la Base de Datos
+REM # Instalacion de la Aplicacion Web como proceso
 REM #==========================================================
-cd C:\forestweb
 
-mongorestore --dbpath mongodb/linux32/data data/mongobackup
-
+copy "forestal.bat" "%USERPROFILE%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\"
 
 echo ::: COPIADO EL SCRIPT DE EJECUCION DE LA APLICACION WEB AL MENU INICIO. OK.
 
